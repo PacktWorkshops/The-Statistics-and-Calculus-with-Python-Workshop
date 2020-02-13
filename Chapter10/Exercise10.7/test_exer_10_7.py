95a354bbc573f762482778c0dd29d7674c83cb9c
Peter Farrell <farrellpolymath@gmail.com>
2020-02-13 10:21:24 -0600

    Add files via upload

---
+++ Chapter10/Exercise10.7/test_exer_10_7.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+import import_ipynb
+from Optimal_Can import *
+
+class Test(unittest.TestCase):
+    
+    def test_derivative(self):
+        self.assertAlmostEqual(derivative(surf_area,4.835),0.0078,4)
+
+    def test_surf_area(self):
+        self.assertAlmostEqual(surf_area(5),220.5,1)
+
+
+if __name__ == '__main__':
+    unittest.main()

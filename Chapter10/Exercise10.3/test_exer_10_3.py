5d388a37413ee2a18ff2431867d3732047c0997d
Peter Farrell <farrellpolymath@gmail.com>
2020-02-13 08:04:28 -0600

    Add files via upload

---
+++ Chapter10/Exercise10.3/test_exer_10_3.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+import import_ipynb
+from Area_Under_Curve import *
+
+class Test(unittest.TestCase):
+    
+    def test_f(self):
+        self.assertAlmostEqual(trap_integral(f,3,4,100),46.75,2)
+
+    def test_g(self):
+        self.assertAlmostEqual(trap_integral(g,0,pi/4,100),2.12,2)
+
+    def test_h(self):
+        self.assertAlmostEqual(trap_integral(h,2,4,100),18.4,1)
+
+
+if __name__ == '__main__':
+    unittest.main()

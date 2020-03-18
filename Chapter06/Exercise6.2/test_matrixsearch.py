d328e6e942bf2e57568cee6e887f428fc224152f
Ajinkya Kolhe <ajinkya.kolhe@yahoo.com>
2020-03-17 19:49:47 -0700

    Added test file for matrixsearch

---
+++ Chapter06/Exercise6.2/test_matrixsearch.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matrixsearch
+import uniitest
+
+class Test(uniitest.TestCase):
+
+    def test_matrixsearch(self):
+        matrix = [
+          [7,  10,  15, 18],
+          [25, 29, 35, 47],
+          [56, 78, 85, 104]
+        ]
+        value = 78
+        # Check for edge cases
+        self.assertIsNotNone(matrix)
+        self.assertIsNotNone(value)
+        result = False
+        if value is None or not matrix:
+                result = False
+
+        # Initialize the variables
+        row = len(matrix)
+        col = len(matrix[0])
+        start = 0
+        end   = row * col - 1
+
+        # Logic for binary search within a sorted matrix
+        while start <= end:
+            mid = (start + end) / 2
+            pointer = matrix[mid/col][mid%col]
+            print(mid/col, mid%col)
+
+            if pointer == value:
+                result = True
+            elif pointer < value:
+                start = mid + 1
+            else:
+                end = mid - 1
+        self.assertTrue(result)

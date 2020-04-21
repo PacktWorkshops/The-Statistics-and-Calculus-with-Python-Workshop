9c2b4a95306e9ea971c116e7df6e090a8a3ea7ba
Peter Farrell <farrellpolymath@gmail.com>
2020-04-21 12:08:36 -0500

    Update Area_of_Hemisphere.py

--- Chapter11/Example11.04/Area_of_Hemisphere.py
+++ Chapter11/Example11.04/Area_of_Hemisphere.py
@@ -44,32 +44,22 @@ def partial_d(f,u,v,w,num):
     except ValueError:
         pass 
 
- 
+def area(f,ax,bx,ay,by,num=1000):
+    """Returns area of parallelogram formed by
+        vectors with given partial derivs"""
+    running_sum = 0
+    dx = (bx-ax)/num
+    dy = (by-ay)/num
+    for i in range(num):
+        for j in range(num):
+            x = ax+i*dx
+            y = ay+j*dy
+            dz_dx=partial_d(f,'x',x,y,num)
+            dz_dy=partial_d(f,'y',x,y,num)
+            try:
+                running_sum += mag(cross([1,0,dz_dx],[0,1,dz_dy]))*dx*dy
+            except:
+                pass
+    return running_sum
 
-def area(f,ax,bx,ay,by,num=10000): 
-
-    """Returns area of parallelogram formed by 
-
-        vectors with given partial derivs""" 
-
-    running_sum = 0 
-
-    dx = (bx-ax)/num 
-
-    dy = (by-ay)/num 
-
-    for i in range(num): 
-
-        for j in range(num): 
-
-            x = ax+i*dx 
-
-            y = ay+j*dy 
-
-            dz_dx=partial_d(f,'x',x,y,num) 
-
-            dz_dy=partial_d(f,'y',x,y,num) 
-
-            running_sum += mag(cross([1,0,dz_dx],[1,0,dz_dy]))*dx*dy 
-
-    return running_sum 
+print("Area of hemisphere:",area(sphere,-1,1,-1,1)) 

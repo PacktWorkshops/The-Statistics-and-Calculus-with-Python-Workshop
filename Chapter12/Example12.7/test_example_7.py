f2b54941c13e94b8b113bf55fdf6c7496922cfb3
Peter Farrell <farrellpolymath@gmail.com>
2020-04-22 17:05:25 -0500

    Update test_example_7.py

--- Chapter12/Example12.7/test_example_7.py
+++ Chapter12/Example12.7/test_example_7.py
@@ -1,12 +1,12 @@
 import unittest
 import sys
 import import_ipynb
-from Runge_Kutta import *
+from Euler import *
 
 class Test(unittest.TestCase):
     
-    def test_deriv(self):
-        self.assertEqual(deriv(7,24),625)
+    def test_euler(self):
+        self.assertEqual(euler(0,1,2,0.5),5.0625)
 
 if __name__ == '__main__':
     unittest.main()

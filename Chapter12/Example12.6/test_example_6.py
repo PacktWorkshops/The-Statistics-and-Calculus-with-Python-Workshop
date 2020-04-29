9462845b8464b19218b57efd3479b817088850a7
Peter Farrell <farrellpolymath@gmail.com>
2020-04-29 14:24:56 -0500

    Update test_example_6.py

--- Chapter12/Example12.6/test_example_6.py
+++ Chapter12/Example12.6/test_example_6.py
@@ -1,12 +1,14 @@
 import unittest
 import sys
 import import_ipynb
-from Time_of_Death import *
+from Salt_Content2 import *
 
 class Test(unittest.TestCase):
     
-    def test_pert(self):
-        self.assertAlmostEqual(pert(33.6,-0.4055,3),9.95,2)
+    def test_salt_content1(self):
+        self.assertAlmostEqual(salt_content1(5*60),183.0769,4)
+    def test_salt_content(self):
+        self.assertAlmostEqual(salt_content(300,100,10000,0.4,20,10),183.0769,4)
 
 if __name__ == '__main__':
     unittest.main()
